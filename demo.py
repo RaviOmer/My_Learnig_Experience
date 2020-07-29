9a3ad7572a7cbc3b4a15812cb6718cb00edc94c4
Ravi Omer <68695886+RaviOmer@users.noreply.github.com>
2020-07-29 17:19:32 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,25 @@
-print('Hello')
+First step to automate a chrome browser by Selenium with Pycharm :------
+
+from selenium import webdriver
+import os
+
+driverlocation = "C:\\Users\\Ravi\\PycharmProjects\\chromedriver.exe"
+
+os.environ["webdriver.chrome.driver"] = driverlocation
+
+driver = webdriver.Chrome(driverlocation)
+driver.get("https://letskodeit.teachable.com/p/practice")
+Searchbyclass = driver.find_element_by_css_selector("#bmwradio")
+searchbycs = driver.find_element_by_xpath("//input[@id='bmwradio']")
+searchby = driver.find_element_by_id("opentab")
+
+if Searchbyclass is not None:
+    print("Successful")
+if searchbycs is not None:
+    print('You code it bro')
+if searchby is not None:
+    print("You are on the right way of learning")
+driver.close()
+print()
+print('THE END')
+
