587e48598c4cbe609ed58d1f93201479caa7b6d0
Ravi Omer <68695886+RaviOmer@users.noreply.github.com>
2020-07-31 15:14:15 +0530

    Update Automate Chrome With Selenium_Pycharm.py

--- Automate Chrome With Selenium_Pycharm.py
+++ Automate Chrome With Selenium_Pycharm.py
@@ -3,23 +3,13 @@ First step to automate a chrome browser by Selenium with Pycharm :------
 from selenium import webdriver
 import os
 
-driverlocation = "C:\\Users\\Ravi\\PycharmProjects\\chromedriver.exe"
+driverlocation = "C:\\Location\\chromedriver.exe"
 
 os.environ["webdriver.chrome.driver"] = driverlocation
 
 driver = webdriver.Chrome(driverlocation)
-driver.get("https://letskodeit.teachable.com/p/practice")
-Searchbyclass = driver.find_element_by_css_selector("#bmwradio")
-searchbycs = driver.find_element_by_xpath("//input[@id='bmwradio']")
-searchby = driver.find_element_by_id("opentab")
+driver.get("https://www.yahoo.com")
 
-if Searchbyclass is not None:
-    print("Successful")
-if searchbycs is not None:
-    print('You code it bro')
-if searchby is not None:
-    print("You are on the right way of learning")
-driver.close()
 print()
 print('THE END')
 
